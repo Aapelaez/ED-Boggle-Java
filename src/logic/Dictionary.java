0fe817eb3d849f89b551cc0f20c4a2890c6a1716
Alfredo Alejandro Peláez García <apelaez@ceis.cujae.edu.cu>
2026-01-09 17:19:36 -0500

    mantener idioma

--- src/logic/Dictionary.java
+++
@@ -1,8 +0,0 @@
-package logic;
-
-public interface Dictionary {
-    void insert(String word);
-    boolean containsWord(String word);
-    boolean containsPrefix(String prefix);
-    int size();
-}
